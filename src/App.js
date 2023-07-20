1753570373a5558b4d7a71579a08e2b032bfdb2a
Sadamramzan <135207789+Sadamramzan@users.noreply.github.com>
2023-07-20 11:20:24 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 // import "./App.css";
-import ToDoList from "./Components/ToDoList";
 import Todo from "./Components/Todo";
 
 function App() {
   return (
     <div className="App">
-      {/* <ToDoList /> */}
       <Todo />
     </div>
   );
